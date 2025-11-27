bbb56b514a1735d9ae506fbb2c2e730538dbcca2
Yash Sawarkar <yash.sawarkar@nimapinfotech.com>
2025-11-27 19:10:19 +0530

    log points toggle added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,11 @@
 -- -- Keymaps are automatically loaded on the VeryLazy event
 -- -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- -- Add any additional keymaps here
---
--- -- Safe import
+
+-----------------------------------------------------------------------------------------------------------------
+---------------------------------                GIt BLAME TOGGLE                --------------------------------
+-----------------------------------------------------------------------------------------------------------------
+
 local ok, gitsigns = pcall(require, "gitsigns")
 if not ok then
   return
@@ -31,3 +34,35 @@ vim.keymap.set("n", "<leader>dL", function()
     end
   end)
 end, { desc = "Set Logpoint" })
+
+-----------------------------------------------------------------------------------------------------------------
+---------------------------------                      DAP                       --------------------------------
+-----------------------------------------------------------------------------------------------------------------
+
+local logpoints = {}
+
+vim.keymap.set("n", "<leader>dL", function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local line = vim.fn.line(".")
+  local key = bufnr .. ":" .. line
+
+  if logpoints[key] then
+    -- Remove logpoint
+    dap.clear_breakpoints(bufnr)
+    logpoints[key] = nil
+    vim.notify("Logpoint removed")
+    return
+  end
+
+  -- Ask for log message
+  vim.ui.input({ prompt = "Logpoint message: " }, function(msg)
+    if not msg or msg == "" then
+      vim.notify("Cancelled")
+      return
+    end
+
+    dap.set_breakpoint(nil, nil, msg)
+    logpoints[key] = true
+    vim.notify("Logpoint added: " .. msg)
+  end)
+end, { desc = "Toggle Logpoint (Add/Remove)" })
