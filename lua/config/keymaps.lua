2b3df5064db6923cefcdb0bf0892e9ac07707f73
Yash Sawarkar <yash.sawarkar@nimapinfotech.com>
2025-11-28 12:11:05 +0530

    toggle button ui changing bug fixed for git blame and log point

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,68 +1,90 @@
--- -- Keymaps are automatically loaded on the VeryLazy event
--- -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- -- Add any additional keymaps here
-
 -----------------------------------------------------------------------------------------------------------------
----------------------------------                GIt BLAME TOGGLE                --------------------------------
+---------------------------------                 DEPENDENCIES                  ---------------------------------
 -----------------------------------------------------------------------------------------------------------------
 
-local ok, gitsigns = pcall(require, "gitsigns")
-if not ok then
+-- Safely load Gitsigns
+local ok_g, gitsigns = pcall(require, "gitsigns")
+if not ok_g then
+  vim.notify("Gitsigns not found", vim.log.levels.WARN)
   return
 end
 
--- Track blame state
-local blame_enabled = false
-
--- Toggle blame with notification
-local function toggle_blame()
-  gitsigns.toggle_current_line_blame()
-  blame_enabled = not blame_enabled
-  vim.notify("Gitsigns Line Blame: " .. (blame_enabled and "ON" or "OFF"))
+-- Safely load DAP
+local ok_dap, dap = pcall(require, "dap")
+if not ok_dap then
+  vim.notify("DAP not found", vim.log.levels.WARN)
+  return
 end
 
--- Keymaps
-vim.keymap.set("n", "<leader>gt", toggle_blame, { desc = "Toggle Line Blame" })
-
--- Logpoint config
-local dap = require("dap")
-
-vim.keymap.set("n", "<leader>dL", function()
-  vim.ui.input({ prompt = "Log Message: " }, function(msg)
-    if msg then
-      dap.set_breakpoint(nil, nil, msg)
-    end
-  end)
-end, { desc = "Set Logpoint" })
+-- Load Snacks for toggle support
+local Snacks = require("snacks")
 
 -----------------------------------------------------------------------------------------------------------------
----------------------------------                      DAP                       --------------------------------
+---------------------------------             GITSIGNS BLAME TOGGLE             ---------------------------------
 -----------------------------------------------------------------------------------------------------------------
 
+-- Track blame state manually
+local blame_enabled = false
+
+Snacks.toggle
+  .option("git_blame", {
+    name = "Git Blame (current line)",
+    get = function()
+      return blame_enabled
+    end,
+    set = function(val)
+      gitsigns.toggle_current_line_blame()
+      blame_enabled = val
+      vim.notify("Git Blame: " .. (val and "ON" or "OFF"))
+    end,
+  })
+  :map("<leader>gt")
+
+-----------------------------------------------------------------------------------------------------------------
+---------------------------------              DAP LOGPOINT TOGGLE              ---------------------------------
+-----------------------------------------------------------------------------------------------------------------
+
+-- Track logpoints per buffer and line
 local logpoints = {}
 
-vim.keymap.set("n", "<leader>dL", function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local line = vim.fn.line(".")
-  local key = bufnr .. ":" .. line
+Snacks.toggle
+  .option("dap_logpoint", {
+    name = "Logpoint",
+    get = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local line = vim.fn.line(".")
+      return logpoints[bufnr .. ":" .. line] == true
+    end,
+    set = function(val)
+      local bufnr = vim.api.nvim_get_current_buf()
+      local line = vim.fn.line(".")
+      local key = bufnr .. ":" .. line
 
-  if logpoints[key] then
-    -- Remove logpoint
-    dap.clear_breakpoints(bufnr)
-    logpoints[key] = nil
-    vim.notify("Logpoint removed")
-    return
-  end
+      if not val then
+        dap.clear_breakpoints(bufnr)
+        logpoints[key] = nil
+        vim.notify("Logpoint removed")
+        return
+      end
 
-  -- Ask for log message
-  vim.ui.input({ prompt = "Logpoint message: " }, function(msg)
-    if not msg or msg == "" then
-      vim.notify("Cancelled")
-      return
-    end
+      vim.ui.input({ prompt = "Logpoint message: " }, function(msg)
+        if not msg or msg == "" then
+          vim.notify("Cancelled")
+          return
+        end
 
-    dap.set_breakpoint(nil, nil, msg)
-    logpoints[key] = true
-    vim.notify("Logpoint added: " .. msg)
-  end)
-end, { desc = "Toggle Logpoint (Add/Remove)" })
+        dap.set_breakpoint(nil, nil, msg)
+        logpoints[key] = true
+        vim.notify("Logpoint added: " .. msg)
+      end)
+    end,
+  })
+  :map("<leader>dL")
+
+-----------------------------------------------------------------------------------------------------------------
+---------------------------------              ADDITIONAL KEYMAPS               ---------------------------------
+-----------------------------------------------------------------------------------------------------------------
+
+-- Add more custom keymaps below this line
+-- Example:
+-- vim.keymap.set("n", "<leader>xx", "<cmd>SomeCommand<cr>", { desc = "Description" })
