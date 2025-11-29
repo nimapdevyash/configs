079f96d935cbd06682ffb87de4a87fd363d67400
Yash Sawarkar <yash.sawarkar@nimapinfotech.com>
2025-11-29 17:36:36 +0530

    added custom dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -51,7 +51,7 @@ return {
     }
 
     for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      button.desc = button.desc .. string.rep(" ", 60 - #button.desc)
       button.key_format = "  %s"
     end
 
